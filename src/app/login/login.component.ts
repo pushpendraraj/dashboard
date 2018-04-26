a35e58bec5da4c32423e9ec746bfd1b9130033ca
Pushpendra Rajput <prerna.g@dimensionindia.com>
2018-04-26 21:21:29 +0530

    login changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogComponent, DialogService } from 'ng2-bootstrap-modal';
 import { RegisterComponent } from '../register/register.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { DataService } from '../data.service';
+import { LocalStorageService } from 'ngx-localstorage';
+import { environment } from '../../environments/environment';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +13,19 @@ import { RegisterComponent } from '../register/register.component';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent extends DialogComponent<LoginModal, boolean> implements OnInit {
-  constructor(dialogService: DialogService) {
+  user = {
+    email:'',
+    password:''
+  }
+  error = '';
+
+  constructor(
+    dialogService: DialogService, 
+    private spinner: NgxSpinnerService, 
+    private dataService: DataService,
+    private _storageService: LocalStorageService,
+    public router: Router
+  ) {
     super(dialogService);
    }
 
@@ -23,6 +40,31 @@ export class LoginComponent extends DialogComponent<LoginModal, boolean> impleme
     if (type === 'register') {
       self.dialogService.addDialog(RegisterComponent, {  }, { closeByClickingOutside: true });
     }
+  }
+
+  login() {
+    this.spinner.show();
+    const userData = this.user;
+    this.dataService.loginUser(userData).subscribe(
+      (loginData) => {
+        if (loginData.login) {
+          this._storageService.set('loggedUser', JSON.stringify(loginData.result), environment.storageKey);
+          this._storageService.set('isLoggedIn', 'true', environment.storageKey);
+          this.close();
+          this.router.navigate(['/']);
+        }else{
+          this.error = '<strong>Sorry</strong> ! Email address OR password incorrect. ';
+          this.spinner.hide();
+        }
+      },
+      (err)=>{
+        this.error = '<strong>Sorry</strong> ! Something went wrong, please try again after some time. ';
+        this.spinner.hide();
+      },
+      () => {
+        this.spinner.hide();
+      }
+    );
   }
 
 }
